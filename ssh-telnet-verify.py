e3873a234ac6e98d92cf55854aa201f03e5d75e6
cursosnetsec <86935257+cursosnetsec@users.noreply.github.com>
2021-10-28 23:13:41 -0300

    Update ssh-telnet-verify.py
    Adicionado s.close()

--- ssh-telnet-verify.py
+++ ssh-telnet-verify.py
@@ -12,18 +12,21 @@ iprangenetwork=ipaddress.ip_network(iprange)
 print("-" * 50)
 print("Scan Alvo:  " + iprange)
 for x in iprangenetwork.hosts():
+    banner=0
     target=0
     s=0
     target = ipaddress.ip_address(x)
     strtarget=str(target)
     print("-" * 50)
     print("Scan para o Server: " + strtarget)
-    for port in range(22,23):
+    for port in range(22,24):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
         result = s.connect_ex((strtarget,port))
-        banner = s.recv(1024)
         if result ==0:
+            banner = s.recv(1024)
             print("Port {} está aberta".format(port))
             print(banner)
+            s.close()
+        else:
             s.close()
